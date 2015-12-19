7a01973390c346b6c2ac40ac599b04c18757ab45
Cody Jennings <cody.a.jennings@gmail.com>
2015-12-19 13:12:24 -0800

    Board tile-split logic; added a full gamestate to spec_helper.rb for testing

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe Board do
+  let(:state) { EX_STATE.dup }
+
   it 'is initializable' do
-    expect(Board.new).not_to be_nil
+    expect(Board.new(state)).not_to be_nil
   end
 end
--- src/board.rb
+++ src/board.rb
@@ -1,5 +1,21 @@
 class Board
-  def initialize
+  def initialize state
+    @state = state
+    @size = @state.game.board.size!
+    @tile_strs = make_tile_strs @state.game.board.tiles
+  end
 
+  private
+
+  def make_tile_strs longstr
+    longstr = longstr.split(/(..)/).to_a.inject([[]]) do |acc, x|
+      if x == ''
+        acc
+      elsif acc.last.length == @size
+        acc << [x]
+      else
+        acc.tap { |obj| obj.last << x }
+      end
+    end.transpose
   end
 end
